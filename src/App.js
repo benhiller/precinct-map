dda3b61dda4292d3c9c337dbdad18d0cd5a86848
bhiller <benhiller@gmail.com>
2020-02-16 18:10:42 -0800

    add mapbox

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import mapboxgl from 'mapbox-gl';
+
 function App() {
+  const mapContainerRef = useRef();
+  const [ state, setState ] = useState({
+    lng: 5,
+    lat: 34,
+    zoom: 2,
+  });
+
+  useEffect(() => {
+    console.log(mapContainerRef)
+    const map = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [state.lng, state.lat],
+      zoom: state.zoom
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="mapContainer" ref={mapContainerRef} />
     </div>
   );
 }
