7b3d53b9fe0c3a44f0dd098e9c900c16ed87c4ea
bhiller <benhiller@gmail.com>
2020-02-17 20:44:44 -0800

    add basic script to convert statement of vote file to JSON, only generates turnout data for now

---
+++ scripts/sov_to_json.py
@@ -0,0 +1,53 @@
+import sys
+import json
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+registered_voters_by_precinct = {}
+ballots_cast_by_precinct = {}
+
+f = open(input_file, 'r')
+for line in f:
+    enc = line[0:26]
+    contest = line[26:82].strip()
+    candidate = line[82:120].strip()
+    precinct = line[120:150].strip()
+    level = line[150:175].strip()
+    voting_method = line[175:205].strip()
+    precinct_num = enc[7:11]
+    # print(enc)
+    # print(contest)
+    # print(candidate)
+    # print(precinct, precinct_num)
+    # print(level)
+    # print(voting_method)
+    if contest == 'REGISTERED VOTERS - TOTAL':
+        registered_voters_by_precinct[precinct_num] = int(enc[11:16])
+    if contest == 'BALLOTS CAST - TOTAL':
+        if precinct_num in ballots_cast_by_precinct:
+            ballots_cast_by_precinct[precinct_num] = ballots_cast_by_precinct[precinct_num] + int(enc[11:16])
+        else:
+            ballots_cast_by_precinct[precinct_num] = int(enc[11:16])
+
+# for p in registered_voters_by_precinct:
+#     if p not in ballots_cast_by_precinct:
+#         print('missing')
+#     if registered_voters_by_precinct[p] < ballots_cast_by_precinct[p]:
+#         print('overvote')
+#
+# for p in ballots_cast_by_precinct:
+#    if p not in registered_voters_by_precinct:
+#        print('missing')
+
+precinct_data = {}
+for p in registered_voters_by_precinct:
+    precinct_data[p] = {
+        'total_voters': registered_voters_by_precinct[p],
+        'ballots_cast': ballots_cast_by_precinct[p],
+    }
+
+f = open(output_file, 'w')
+precinct_json = json.dumps(precinct_data)
+f.write(precinct_json)
+f.close()
