1b3701128b73e376c8e9d39b50a1c46dc5997b05
bhiller <benhiller@gmail.com>
2020-02-17 20:56:30 -0800

    improve map initialization code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 import mapboxgl from 'mapbox-gl';
@@ -11,57 +11,66 @@ const LONG = -122.444;
 const ZOOM = 12
 
 function App() {
+  const [map, setMap] = useState(null);
   const mapContainerRef = useRef();
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      style: 'mapbox://styles/mapbox/light-v10',
-      center: [LONG, LAT],
-      zoom: ZOOM,
-    });
-
-    map.on('load', function() {
-      map.addSource('precincts', {
-        type: 'geojson',
-        data: precinctData,
+    const initializeMap = ((setMap, mapContainerRef) => {
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: 'mapbox://styles/mapbox/light-v10',
+        center: [LONG, LAT],
+        zoom: ZOOM,
       });
 
-      const expression = ['match', ['get', 'PREC_2012']];
+      map.on('load', function() {
+        map.addSource('precincts', {
+          type: 'geojson',
+          data: precinctData,
+        });
 
-      for (let idx in precinctData.features) {
-        const precinct = precinctData.features[idx];
-        if (!precinct['properties']['PREC_2012']) {
-          console.log('no precinct', precinct);
-          continue;
+        const expression = ['match', ['get', 'PREC_2012']];
+
+        for (let idx in precinctData.features) {
+          const precinct = precinctData.features[idx];
+          if (!precinct['properties']['PREC_2012']) {
+            console.log('no precinct', precinct);
+            continue;
+          }
+          const precinctTurnoutData = turnoutData[precinct['properties']['PREC_2012']];
+          let color;
+          if (!precinctTurnoutData) {
+            color = 'rgba(255,0,0,0.75)';
+            console.log('no turnout data', precinct);
+          } else if (precinctTurnoutData['total_voters'] === 0) {
+            color = 'rgba(0,0,255,0.75)';
+          } else {
+            var green = (1 - (precinctTurnoutData['ballots_cast'] / precinctTurnoutData['total_voters'])) * 255;
+            color = 'rgba(' + 0 + ', ' + green + ', ' + 0 + ', 0.75)';
+          }
+          expression.push(precinct['properties']['PREC_2012'], color);
         }
-        const precinctTurnoutData = turnoutData[precinct['properties']['PREC_2012']];
-        let color;
-        if (!precinctTurnoutData) {
-          color = 'rgba(255,0,0,0.75)';
-          console.log('no turnout data', precinct);
-        } else if (precinctTurnoutData['total_voters'] === 0) {
-          color = 'rgba(0,0,255,0.75)';
-        } else {
-          var green = (1 - (precinctTurnoutData['ballots_cast'] / precinctTurnoutData['total_voters'])) * 255;
-          color = 'rgba(' + 0 + ', ' + green + ', ' + 0 + ', 0.75)';
-        }
-        expression.push(precinct['properties']['PREC_2012'], color);
-      }
 
-      expression.push('rgba(0,0,0,0)');
+        expression.push('rgba(0,0,0,0)');
 
-      map.addLayer({
-        id: 'precinct-borders',
-        type: 'fill',
-        source: 'precincts',
-        paint: {
-          'fill-color': expression,
-        },
-        filter: ['==', '$type', 'Polygon'],
+        map.addLayer({
+          id: 'precinct-borders',
+          type: 'fill',
+          source: 'precincts',
+          paint: {
+            'fill-color': expression,
+          },
+          filter: ['==', '$type', 'Polygon'],
+        });
       });
+
+
     });
-  });
+
+    if (!map) {
+        initializeMap(setMap, mapContainerRef);
+    }
+  }, [map]);
 
   return (
     <div className="app">
