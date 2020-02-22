df37fb8ff014a80eaab5b3dedfb085e9b69b20f4
bhiller <benhiller@gmail.com>
2020-02-22 11:52:25 -0800

    allow changing contest

--- src/App.js
+++ src/App.js
@@ -30,10 +30,16 @@ const COLORS = [
 
 const THRESHOLDS = [-25, -20, -15, -10, -1, 1, 10, 15, 20, 25];
 
-const CONTEST = 'President - DEM';
+const DEFAULT_CONTEST = 'President - DEM';
 
 const useStyles = createUseStyles({
   app: {},
+  contestControl: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    zIndex: '1 !important',
+  },
   mapContainer: {
     position: 'absolute',
     top: 0,
@@ -46,11 +52,13 @@ const useStyles = createUseStyles({
 const computeOverallResults = (turnoutData, contest) => {
   const results = {};
   for (const precinct of Object.keys(turnoutData)) {
-    for (const candidate of Object.keys(turnoutData[precinct][contest])) {
-      if (candidate in results) {
-        results[candidate] += turnoutData[precinct][contest][candidate];
-      } else {
-        results[candidate] = turnoutData[precinct][contest][candidate];
+    if (contest in turnoutData[precinct]) {
+      for (const candidate of Object.keys(turnoutData[precinct][contest])) {
+        if (candidate in results) {
+          results[candidate] += turnoutData[precinct][contest][candidate];
+        } else {
+          results[candidate] = turnoutData[precinct][contest][candidate];
+        }
       }
     }
   }
@@ -59,7 +67,9 @@ const computeOverallResults = (turnoutData, contest) => {
 
 function App() {
   const classes = useStyles();
+  const [contest, setContest] = useState(DEFAULT_CONTEST);
   const [map, setMap] = useState(null);
+  const [mapLoaded, setMapLoaded] = useState(false);
   const [tooltipPrecinct, setTooltipPrecinct] = useState(null);
   const [turnoutData, setTurnoutData] = useState(null);
   const [fetchingTurnoutData, setFetchingTurnoutData] = useState(false);
@@ -122,12 +132,12 @@ function App() {
         tooltipTurnoutData = turnoutData[tooltipPrecinctNum];
       }
 
-      if (tooltipTurnoutData) {
+      if (tooltipTurnoutData && contest in tooltipTurnoutData) {
         ReactDOM.render(
           React.createElement(Tooltip, {
             precinctData: tooltipPrecinct,
             turnoutData: tooltipTurnoutData,
-            contest: CONTEST,
+            contest: contest,
           }),
           tooltipContainer,
         );
@@ -135,15 +145,11 @@ function App() {
         ReactDOM.unmountComponentAtNode(tooltipContainer);
       }
     }
-  }, [turnoutData, tooltipContainer, tooltipPrecinct]);
+  }, [turnoutData, tooltipContainer, tooltipPrecinct, contest]);
 
   useEffect(() => {
-    if (!tooltipContainer) {
-      setTooltipContainer(document.createElement('div'));
-    }
-  }, [tooltipContainer]);
+    const tooltipDiv = document.createElement('div');
 
-  useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: 'mapbox://styles/mapbox/light-v10',
@@ -151,27 +157,7 @@ function App() {
       zoom: ZOOM,
     });
 
-    setMap(map);
-  }, []);
-
-  useEffect(() => {
-    if (!map) {
-      return;
-    }
-
-    if (!precinctData) {
-      return;
-    }
-
-    if (!turnoutData) {
-      return;
-    }
-
-    if (!tooltipContainer) {
-      return;
-    }
-
-    const tooltip = new mapboxgl.Marker(tooltipContainer, {
+    const tooltip = new mapboxgl.Marker(tooltipDiv, {
       offset: [0, -70],
     })
       .setLngLat([0, 0])
@@ -184,91 +170,139 @@ function App() {
       setTooltipPrecinct(filteredFeature);
     });
 
-    map.on('load', function() {
-      map.addSource('precincts', {
-        type: 'geojson',
-        data: precinctData,
-      });
-
-      const expression = ['match', ['get', 'PREC_2012']];
-
-      const overallResults = computeOverallResults(turnoutData, CONTEST);
-      const topCandidates = [];
-      for (const candidate of Object.keys(overallResults)) {
-        const votes = overallResults[candidate];
-        if (topCandidates.length < 2) {
-          topCandidates.push({ candidate, votes });
-        } else {
-          if (votes > topCandidates[0].votes) {
-            if (topCandidates[0].votes > topCandidates[1].votes) {
-              topCandidates[1] = topCandidates[0];
-            }
-            topCandidates[0] = { candidate, votes };
-          } else if (votes > topCandidates[1].votes) {
-            if (topCandidates[1].votes > topCandidates[0].votes) {
-              topCandidates[0] = topCandidates[1];
-            }
-            topCandidates[1] = { candidate, votes };
-          }
-        }
-      }
-
-      for (let idx in precinctData.features) {
-        const precinct = precinctData.features[idx];
-        if (!precinct['properties']['PREC_2012']) {
-          console.log('no precinct', precinct);
-          continue;
-        }
-        const precinctTurnoutData =
-          turnoutData[precinct['properties']['PREC_2012']];
-        if (!precinctTurnoutData) {
-          console.log('no turnout data', precinct);
-          continue;
-        } else if (precinctTurnoutData['total_voters'] === 0) {
-          continue;
-        }
-
-        const candidate0Votes =
-          precinctTurnoutData[CONTEST][topCandidates[0].candidate];
-        const candidate1Votes =
-          precinctTurnoutData[CONTEST][topCandidates[1].candidate];
-        const totalVotes = Object.keys(precinctTurnoutData[CONTEST]).reduce(
-          (total, candidate) => total + precinctTurnoutData[CONTEST][candidate],
-          0,
-        );
-
-        // TODO: Need to check logic makes sense here
-        const margin = ((candidate0Votes - candidate1Votes) / totalVotes) * 100;
-        let color;
-        for (let i = 0; i < THRESHOLDS.length; i++) {
-          if (margin <= THRESHOLDS[i]) {
-            color = COLORS[i];
-            break;
-          }
-          if (i === THRESHOLDS.length - 1) {
-            color = COLORS[i + 1];
-          }
-        }
-
-        expression.push(precinct['properties']['PREC_2012'], color);
-      }
-
-      expression.push('rgba(0,0,0,0)');
-
-      map.addLayer({
-        id: 'precinct-borders',
-        type: 'fill',
-        source: 'precincts',
-        paint: {
-          'fill-color': expression,
-        },
-        filter: ['==', '$type', 'Polygon'],
-      });
+    map.on('load', () => {
+      setMapLoaded(true);
     });
-  }, [map, precinctData, turnoutData, tooltipContainer]);
+
+    setMap(map);
+    setTooltipContainer(tooltipDiv);
+  }, []);
+
+  useEffect(() => {
+    if (!mapLoaded) {
+      return;
+    }
+    if (!precinctData) {
+      return;
+    }
+
+    map.addSource('precincts', {
+      type: 'geojson',
+      data: precinctData,
+    });
+  }, [precinctData, map, mapLoaded]);
+
+  useEffect(() => {
+    if (!mapLoaded) {
+      return;
+    }
+
+    if (!precinctData) {
+      return;
+    }
+
+    if (!turnoutData) {
+      return;
+    }
+
+    if (map.getLayer('precinct-borders')) {
+      map.removeLayer('precinct-borders');
+    }
+
+    const expression = ['match', ['get', 'PREC_2012']];
+
+    const overallResults = computeOverallResults(turnoutData, contest);
+    const topCandidates = [];
+    for (const candidate of Object.keys(overallResults)) {
+      const votes = overallResults[candidate];
+      if (topCandidates.length < 2) {
+        topCandidates.push({ candidate, votes });
+      } else {
+        if (votes > topCandidates[0].votes) {
+          if (topCandidates[0].votes > topCandidates[1].votes) {
+            topCandidates[1] = topCandidates[0];
+          }
+          topCandidates[0] = { candidate, votes };
+        } else if (votes > topCandidates[1].votes) {
+          if (topCandidates[1].votes > topCandidates[0].votes) {
+            topCandidates[0] = topCandidates[1];
+          }
+          topCandidates[1] = { candidate, votes };
+        }
+      }
+    }
+
+    for (let idx in precinctData.features) {
+      const precinct = precinctData.features[idx];
+      if (!precinct['properties']['PREC_2012']) {
+        console.log('no precinct', precinct);
+        continue;
+      }
+      const precinctTurnoutData =
+        turnoutData[precinct['properties']['PREC_2012']];
+      if (!precinctTurnoutData) {
+        console.log('no turnout data', precinct);
+        continue;
+      } else if (precinctTurnoutData['total_voters'] === 0) {
+        continue;
+      } else if (!(contest in precinctTurnoutData)) {
+        continue;
+      }
+
+      const candidate0Votes =
+        precinctTurnoutData[contest][topCandidates[0].candidate];
+      const candidate1Votes =
+        precinctTurnoutData[contest][topCandidates[1].candidate];
+      const totalVotes = Object.keys(precinctTurnoutData[contest]).reduce(
+        (total, candidate) => total + precinctTurnoutData[contest][candidate],
+        0,
+      );
+
+      // TODO: Need to check logic makes sense here
+      const margin = ((candidate0Votes - candidate1Votes) / totalVotes) * 100;
+      let color;
+      for (let i = 0; i < THRESHOLDS.length; i++) {
+        if (margin <= THRESHOLDS[i]) {
+          color = COLORS[i];
+          break;
+        }
+        if (i === THRESHOLDS.length - 1) {
+          color = COLORS[i + 1];
+        }
+      }
+
+      expression.push(precinct['properties']['PREC_2012'], color);
+    }
+
+    expression.push('rgba(0,0,0,0)');
+
+    map.addLayer({
+      id: 'precinct-borders',
+      type: 'fill',
+      source: 'precincts',
+      paint: {
+        'fill-color': expression,
+      },
+      filter: ['==', '$type', 'Polygon'],
+    });
+  }, [map, mapLoaded, precinctData, turnoutData, contest]);
+
+  let contests = [];
+  if (turnoutData) {
+    contests = Object.keys(turnoutData[Object.keys(turnoutData)[0]]);
+  }
 
   return (
     <div className={classes.app}>
+      <div className={classes.contestControl}>
+        <select onChange={e => setContest(e.target.value)} value={contest}>
+          {contests.map(innerContest => (
+            <option key={innerContest} value={innerContest}>
+              {innerContest}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className={classes.mapContainer} ref={mapContainerRef} />
     </div>
   );
